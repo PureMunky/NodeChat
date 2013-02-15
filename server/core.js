65b7baf2703e0998537824efdc7797b588c7e6ae
Phil Corbett <corbett.phil@gmail.com>
2013-02-15 06:37:50 -0500

    add some basic web socket stuff

--- server/core.js
+++ server/core.js
@@ -1,5 +1,25 @@
 var http = require('http'),
+	WebSocket = require('ws'),		// npm install ws
+	ws = new WebSocket('ws://localhost'),
+	port = 1337,
 	log = [];
+
+ws.on('open', function () {
+	ws.send('welcome');
+});
+
+ws.on('message', function (data, flags){
+	switch (data.action) {
+		case 'get':
+			ws.send(log);
+			break;
+		case 'send':
+			log.push(data.text)
+			break;
+		default:
+			ws.send(log[log.length - 1]);
+	}
+});
 
 http.createServer(function (request, response) {
  	response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -12,4 +32,4 @@ http.createServer(function (request, response) {
 	response.end('End of List\n');
 	
 	//writeServerLog(request);
-}).listen('getlist');
+}).listen(port);
